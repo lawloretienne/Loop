ababa2b0495b854d67d22486bc8bd8425ea81a6f
Etienne Lawlor <etienne@shopsavvy.com>
2015-10-19 17:22:06 -0700

    Updated Timber

--- app/src/main/java/com/etiennelawlor/loop/LoopApplication.java
+++ app/src/main/java/com/etiennelawlor/loop/LoopApplication.java
@@ -74,9 +74,15 @@ public class LoopApplication extends Application {
 
     private void initializeTimber(){
         if (BuildConfig.DEBUG) {
-            Timber.plant(new Timber.DebugTree());
+            Timber.plant(new Timber.DebugTree() {
+                // Add the line number to the tag
+                @Override
+                protected String createStackElementTag(StackTraceElement element) {
+                    return super.createStackElementTag(element) + ":" + element.getLineNumber();
+                }
+            });
         } else {
-            Timber.plant(new CrashReportingTree());
+            Timber.plant(new ReleaseTree());
         }
     }
 
@@ -90,14 +96,23 @@ public class LoopApplication extends Application {
     // region Inner Classes
 
     /** A tree which logs important information for crash reporting. */
-    private static class CrashReportingTree extends Timber.Tree {
+    private static class ReleaseTree extends Timber.Tree {
+
+        private static final int MAX_LOG_LENGTH = 4000;
         @Override
-        protected void log(int priority, String tag, String message, Throwable t) {
-            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
-                return;
+        protected boolean isLoggable(int priority) {
+            if(priority == Log.VERBOSE || priority == Log.DEBUG){
+                return false;
             }
 
-//            FakeCrashLibrary.log(priority, tag, message);
+            // Only log WARN, INFO, ERROR, WTF
+            return true;
+        }
+
+        @Override
+        protected void log(int priority, String tag, String message, Throwable t) {
+            if(isLoggable(priority)){
+                //            FakeCrashLibrary.log(priority, tag, message);
 //
 //            if (t != null) {
 //                if (priority == Log.ERROR) {
@@ -107,13 +122,40 @@ public class LoopApplication extends Application {
 //                }
 //            }
 
-            Crashlytics.log(priority, tag, message);
+                Crashlytics.log(priority, tag, message);
 
-            if (t != null) {
-                if (priority == Log.ERROR) {
-                    Crashlytics.logException(t);
-                } else if (priority == Log.INFO) {
-                    Crashlytics.log(message);
+                if (t != null) {
+                    if (priority == Log.ERROR) {
+                        Crashlytics.logException(t);
+                    } else if (priority == Log.INFO) {
+                        Crashlytics.log(message);
+                    }
+                }
+
+                // Message is short enough, does not need to be broken into chunks
+                if(message.length() < MAX_LOG_LENGTH){
+                    if(priority == Log.ASSERT) {
+                        Log.wtf(tag, message);
+                    } else {
+                        Log.println(priority, tag, message);
+                    }
+                    return;
+                }
+
+                // Split by line, then ensure each line can fit into Log's maximum length
+                for(int i=0, length = message.length(); i<length; i++) {
+                    int newline = message.indexOf('\n', i);
+                    newline = newline != -1 ? newline : length;
+                    do {
+                        int end = Math.min(newline, i + MAX_LOG_LENGTH);
+                        String part = message.substring(i, end);
+                        if(priority == Log.ASSERT) {
+                            Log.wtf(tag, part);
+                        } else {
+                            Log.println(priority, tag, part);
+                        }
+                        i = end;
+                    } while (i < newline);
                 }
             }
         }
